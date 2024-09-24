f1ac4dd31a92bb65bdc45b617ea41a6d85f3d942
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-09-24 18:03:32 -0300

    fix express-flash middleware order

--- index.js
+++ index.js
@@ -12,12 +12,6 @@ initialize(passport);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(flash());
-app.use((req, res, next) => {
-	res.locals.messages = req.flash();
-	next();
-});
-
 // Session setup
 app.use(
 	session({
@@ -26,6 +20,11 @@ app.use(
 		saveUninitialized: false,
 	})
 );
+app.use(flash());
+app.use((req, res, next) => {
+	res.locals.messages = req.flash();
+	next();
+});
 
 // Passport setup
 app.use(passport.initialize());

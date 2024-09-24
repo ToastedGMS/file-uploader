707311eb1625d9129e96cefbd33622c03952cb99
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-09-24 20:38:55 -0300

    added user authentication function

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,25 @@
+const passport = require('passport');
+
 function getLoginView(req, res) {
 	res.render('login');
 }
 
-module.exports = { getLoginView };
+async function logInUser(req, res, next) {
+	passport.authenticate('local', (err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			req.flash('error', info.message);
+			return res.redirect('/login');
+		}
+		req.logIn(user, (err) => {
+			if (err) {
+				return next(err);
+			}
+			return res.redirect('/dashboard');
+		});
+	})(req, res, next);
+}
+
+module.exports = { getLoginView, logInUser };
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,9 @@
-const { getLoginView } = require('../controllers/loginController');
+const { getLoginView, logInUser } = require('../controllers/loginController');
 
 const router = require('express').Router();
 
 router.get('/', getLoginView);
+
+router.post('/', logInUser);
 
 module.exports = router;

0752409e36829e0d7f0e732d99e172d4a49ad505
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-09-24 19:32:25 -0300

    add logic for signing up users

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,5 +1,25 @@
+const { addUserToDatabase } = require('../services/databaseService');
+
 function getSignupView(req, res) {
 	res.render('signup');
 }
 
-module.exports = { getSignupView };
+async function createUser(req, res) {
+	const { email, password, password2 } = req.body;
+
+	if (password != password2) {
+		req.flash('error', 'Passwords do not match');
+		return res.render('signup');
+	}
+
+	try {
+		const newUser = await addUserToDatabase(email, password);
+		req.flash('success', 'User created successfully!');
+		res.redirect('/login');
+	} catch (error) {
+		req.flash('error', 'User creation failed.');
+		res.render('signup');
+	}
+}
+
+module.exports = { getSignupView, createUser };
--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,12 @@
-const { getSignupView } = require('../controllers/signupController');
+const {
+	getSignupView,
+	createUser,
+} = require('../controllers/signupController');
 
 const router = require('express').Router();
 
 router.get('/', getSignupView);
+
+router.post('/', createUser);
 
 module.exports = router;

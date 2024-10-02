8cffed1591ae8041c5a5a5086e725a2a350975bc
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-10-02 18:50:39 -0300

    refactor folder browsing logic to use folder stack to keep track of history

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -4,13 +4,38 @@ const util = require('util');
 // Convert fs.readdir into a function that returns a promise (for using async/await consistently)
 const readdir = util.promisify(fs.readdir);
 
+async function getNewFolder(req, res) {
+	// Push the current folder onto the stack before navigating
+	folderHistory.push(currentFolder);
+	currentFolder = `${currentFolder}/${req.query.folderName}`;
+
+	await getFolderView(req, res);
+}
+
+async function getPreviousFolder(req, res) {
+	// Check if there's a previous folder in the stack
+	if (folderHistory.length > 0) {
+		// Pop the last folder from the stack and set it as the current folder
+		currentFolder = folderHistory.pop();
+		console.log('Navigating to previous folder:', currentFolder);
+	}
+
+	await getFolderView(req, res);
+}
+
 async function getFolderView(req, res) {
 	if (req.isAuthenticated()) {
 		try {
 			const files = await getFolderContent();
 			res.render('folder', { files });
 		} catch (err) {
-			res.status(500).send('Error loading folder contents: ' + err);
+			if (err.code === 'ENOENT') {
+				currentFolder = './uploads';
+				folderHistory = []; // Clear the folder history
+				return await getFolderView(req, res);
+			} else {
+				res.status(500).send('Error loading folder contents: ' + err);
+			}
 		}
 	} else {
 		res.redirect('/login');
@@ -18,14 +43,12 @@ async function getFolderView(req, res) {
 }
 
 async function getFolderContent() {
-	const folder = currentFolder;
-
 	try {
-		const files = await readdir(folder);
+		const files = await readdir(currentFolder);
 		return files;
 	} catch (err) {
 		throw new Error('Unable to read folder: ' + err);
 	}
 }
 
-module.exports = { getFolderView, getFolderContent };
+module.exports = { getNewFolder, getPreviousFolder, getFolderView };

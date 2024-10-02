288ce6ab34432fee1eb16f3d7f7a2da59e5bcb45
ToastedGMS <gabrielsiqueira3052@gmail.com>
2024-10-02 18:51:02 -0300

    add logic for browsing folders back and forth

--- routes/folders.js
+++ routes/folders.js
@@ -1,7 +1,15 @@
-const { getFolderView } = require('../controllers/folderController');
+const {
+	getFolderView,
+	getNewFolder,
+	getPreviousFolder,
+} = require('../controllers/folderController');
 
 const router = require('express').Router();
 
 router.get('/', getFolderView);
+
+router.get('/selection', getNewFolder);
+
+router.get('/previous', getPreviousFolder);
 
 module.exports = router;
